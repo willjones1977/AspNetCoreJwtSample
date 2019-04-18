6bd8f0627228acf03572ee343906558e3bccc878
Will Jones <will.jones@silverlinesolutions.com>
2019-04-18 11:08:53 -0400

    Add Get method to invoices controller for testing

--- ASPNetCoreJWTSample/Controllers/InvoiceController.cs
+++ ASPNetCoreJWTSample/Controllers/InvoiceController.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNetCoreJWTSample.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    // [Route("api/[controller]/[action]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class InvoiceController : ControllerBase
     {
@@ -24,6 +25,23 @@ namespace ASPNetCoreJWTSample.Controllers
         {
             var userIdClaim = HttpContext.User.Claims.Where(x => x.Type == "userid").SingleOrDefault();
             return Ok($"Your User ID is {userIdClaim.Value} and you can create invoices!");
+        }
+
+        [Authorize(Roles = "Administrator,Accountant")]
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if(id == 1)
+            {
+                return Ok($"Here is the invoice number 1!");
+                
+            }
+            if (id == 2)
+            {
+                return Ok($"Here is the invoice number 2!");
+
+            }
+            return NotFound();
         }
 
         [Authorize(Roles = "Administrator")]
